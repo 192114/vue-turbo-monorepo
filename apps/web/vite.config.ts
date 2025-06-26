918f6061c8cd8fe11e06da1fbb9e9771b2e6ee63
shadow <shadowsun192114@gmail.com>
2025-06-26 22:23:01 +0800

    优化工程

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,11 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+
+  const alias = {
+    '@': fileURLToPath(new URL('./src', import.meta.url)),
+  }
+
+  if (isDev) {
+    alias['@shadow/ui'] = fileURLToPath(new URL('../../packages/ui/src', import.meta.url))
+  } else {
+    alias['@shadow/ui'] = fileURLToPath(new URL('../../packages/ui/dist', import.meta.url))
+  }
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias,
     },
-  },
+  }
 })
